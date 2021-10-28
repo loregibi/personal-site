d2be527f757d5bd1e0de1b32e1fa4951ca7ac5c8
Lorenzo Gibilisco <81555528+loregibi@users.noreply.github.com>
2021-10-28 16:55:40 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ function sendMail(params) {
   var tempParams = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
-    subject: document.getElementById("subject").value,
+    subject: document.getElementById("object").value,
     message: document.getElementById("message").value,
   };
 
